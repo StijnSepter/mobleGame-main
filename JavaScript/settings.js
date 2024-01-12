8a469fdd6b6b35b321dfeb627cf7bed94198222a
stijn <stijnsepter@gmail.com>
2024-01-12 14:40:47 +0100

    commit2

--- JavaScript/settings.js
+++ JavaScript/settings.js
@@ -2,7 +2,9 @@ gearJS = document.getElementById("gear");
 gearJS.addEventListener("click", gearSettings);
 const canvas = document.getElementById("canvas");
 const settings = document.getElementById("settings");
+
 function gearSettings() {
+    console.log("gearSettings");
     canvas.style.display = "none";
     settings.style.display = "block";
     element.classList.toggle("mystyle");
